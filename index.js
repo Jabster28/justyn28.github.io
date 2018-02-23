02c85f1ae1f408cfef5bfd7e5cb8d5000b47fa76
Your Name <Justyn>
2018-02-23 21:13:36 +0000

    Added a redirect after auth page, and some Sign Up HTML & JS

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const auth = firebase.auth();
+const
 function siweap() {
   const email = document.getElementById('email').value;
 const password = document.getElementById('password').value;
 const txtemail = email
 const txtpass = password
 const promise = auth.signInWithEmailAndPassword(email, txtpass);
+promise.catch(e => console.log(e.message));
+  console.log("email is:");
+  console.log(email, txtemail);
+    console.log("password is:");
+  console.log(password, txtpass);
+}
+function suweap() {
+  const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+const txtemail = email
+const txtpass = password
+const promise = auth.createUserWithEmailAndPassword(email, txtpass);
 promise.catch(e => console.log(e.message));
   console.log("email is:");
   console.log(email, txtemail);
