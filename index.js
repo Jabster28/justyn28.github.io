ab3dedcf2cf079e1b62f17a0eb1547ca4deb8a33
Your Name <Justyn>
2018-02-23 20:53:27 +0000

    Fixed login JS

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const auth = firebase.auth();
 function siweap() {
-  const email = document.getElementById("email");
-const password = document.getElementById("password");
-const txtemail = email.value
-const txtpass = password.value
+  const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+const txtemail = email
+const txtpass = password
 const promise = auth.signInWithEmailAndPassword(email, txtpass);
 promise.catch(e => console.log(e.message));
   console.log("email is:");
